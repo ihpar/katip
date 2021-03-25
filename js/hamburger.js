98a62b6791b710cb647d857e3c8f4de6240b4fde
ihpar <ismail@cs.deu.edu.tr>
2021-03-25 21:20:58 +0300

    tidy up

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,12 @@
-let hamburger = document.querySelector(".hamburger");
-let userMenu = document.querySelector(".user-menu-wrap");
-hamburger.addEventListener(
-  "click",
-  function () {
-    this.classList.toggle("is-active");
-    userMenu.classList.toggle("user-menu-visible");
-  },
-  false
-);
+(function () {
+  let hamburger = document.querySelector(".hamburger");
+  let userMenu = document.querySelector(".user-menu-wrap");
+  hamburger.addEventListener(
+    "click",
+    function () {
+      this.classList.toggle("is-active");
+      userMenu.classList.toggle("user-menu-visible");
+    },
+    false
+  );
+})();
