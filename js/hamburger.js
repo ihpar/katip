dfefe7d5baaa7e7fbbe5420cf25842c37d6f561b
ihpar <ismail@cs.deu.edu.tr>
2021-03-25 14:54:26 +0300

    display grid

---
+++ js/hamburger.js
@@ -0,0 +1,10 @@
+let hamburger = document.querySelector(".hamburger");
+let userMenu = document.querySelector(".user-menu-wrap");
+hamburger.addEventListener(
+  "click",
+  function () {
+    this.classList.toggle("is-active");
+    userMenu.classList.toggle("user-menu-visible");
+  },
+  false
+);
